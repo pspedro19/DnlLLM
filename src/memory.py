45f5f7c89b74adb311acaa8e58888a30f5a1cbdd
Pedro <96pedroelias96@gmail.com>
2024-04-13 09:20:34 -0500

    nnn

--- src/memory.py
+++ src/memory.py
@@ -1,12 +1,14 @@
-from sentence_transformers import SentenceTransformer, util
 import json
-from pathlib import Path
+import torch
+from transformers import BertTokenizer, BertModel
+from sklearn.metrics.pairwise import cosine_similarity
 
 class EnhancedVectorMemory:
     def __init__(self, memory_file):
         self.memory_file = memory_file
         self.memory = self.load_memory()
-        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.model = BertModel.from_pretrained('bert-base-uncased')
 
     def load_memory(self):
         memory = []
@@ -26,12 +28,17 @@ class EnhancedVectorMemory:
         self.save_memory()
 
     def get_closest_memory(self, query):
-        query_embedding = self.model.encode(query)
-        closest = None
+        query_tokens = self.tokenizer(query, return_tensors='pt', truncation=True, max_length=512)
+        with torch.no_grad():
+            query_embedding = self.model(**query_tokens).last_hidden_state.mean(dim=1)
+        
         highest_similarity = -1
+        closest = None
         for entry in self.memory:
-            value_embedding = self.model.encode(entry["Pregunta"])
-            similarity = util.pytorch_cos_sim(query_embedding, value_embedding)
+            entry_tokens = self.tokenizer(entry["Pregunta"], return_tensors='pt', truncation=True, max_length=512)
+            with torch.no_grad():
+                entry_embedding = self.model(**entry_tokens).last_hidden_state.mean(dim=1)
+            similarity = cosine_similarity(query_embedding, entry_embedding)
             if similarity > highest_similarity:
                 highest_similarity = similarity
                 closest = entry
